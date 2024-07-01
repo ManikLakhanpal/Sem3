26b0a852cab8e344c13361df5cabb9dda83a5ec3
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2024-07-01 13:12:15 +0530

    Day complete

---
+++ FEE/01-07-2024/BookReview/src/App.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // prevents the original default value to be applied on the data hook
+    const name = event.target.elements.name.value;
+    const review = event.target.elements.review.value;
+    setData((prevData) => [
+      ...prevData, // Includes the previous value in the data array.
+      {
+        name: name,
+        review: review,
+      },
+    ]);
+  };
+
+  return (
+    <div className="main">
+      <div className="reviewInput">
+        <form onSubmit={handleSubmit}>
+          <h4>Name of the book</h4>
+          <textarea
+            id="name"
+            type="text"
+            placeholder="enter name"
+            name="name"
+            required="true"
+            autoFocus="true"
+            wrap="hard"
+          />
+          <h4>Your Review</h4>
+          <textarea
+            id="review"
+            type="text"
+            placeholder="enter review"
+            name="review"
+            required="true"
+            className="reviewText"
+            wrap="hard"
+          />
+          <br />
+          <input className="submitButton" type="submit" />
+        </form>
+      </div>
+
+      <div className="reviews">
+        {data.map((item) => {
+          return (
+            <div className="review">
+              <h1>{item.name}</h1>
+              <p>{item.review}</p>
+              <hr />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
